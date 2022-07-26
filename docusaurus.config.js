66031fd5c2fa8d8918912bb7444c6b559f61273b
guycherno <72074365+guycherno@users.noreply.github.com>
2022-07-26 12:36:18 +0300

    Update docusaurus.config.js
    Change tag line.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Four legs good. Two legs bad.',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   //onBrokenLinks: 'throw',
