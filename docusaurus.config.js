a44f52e71406c36bda61d58dba11dc18a28e8082
houdini91 <mdstrauss91@gmail.com>
2022-07-27 12:50:27 +0300

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,8 @@ var branch = require('child_process')
   .execSync('git branch --show-current')
   .toString().trim();
 branch = branch ? branch : process.env.HEAD?.toString() ?? "";
-var isPullRequest = process.env.PULL_REQUEST;
-
+var isPullRequest = Boolean(process.env.PULL_REQUEST);
+console.log("###########", branch, isPullRequest)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -25,7 +25,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
-
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
