62db8c46b5c2317ce4bc07f4b7ea68da5e8f4676
Barak Brudo <mr.lightning@gmail.com>
2022-08-03 10:33:21 +0300

    adding redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,13 +29,14 @@ function HomepageHeader() {
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
+    <Redirect to="/docs/docs/overview"/>
+    // <Layout
+    //   title={`Hello from ${siteConfig.title}`}
+    //   description="Description will go into a meta tag in <head />">
+    //   <HomepageHeader />
+    //   <main>
+    //     <HomepageFeatures />
+    //   </main>
+    // </Layout>
   );
 }
