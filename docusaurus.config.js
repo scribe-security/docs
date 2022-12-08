31b07d3410e55a166d7ed0244adc83114a88a308
Barak Brudo <mr.lightning@gmail.com>
2022-12-08 16:53:20 +0200

    scribe hub slsa policies

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,14 +60,15 @@ const config = {
                 "readingoutput.md",
                 "faq.md",
                 "overview.md" ,
+                "slsapolicies.md",
                 "README.md",
               ],
             ],
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
