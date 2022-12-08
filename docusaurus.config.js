7bcfd10263c527739cd0dcdade0aacb5a90dcc2b
MrBLightning <44550952+MrBLightning@users.noreply.github.com>
2022-12-08 19:30:33 +0200

    Barak slsapolicies (#102)
    * scribe hub slsa policies
    
    * scribe hub slsa policies
    
    * update slsa version
    
    Co-authored-by: houdini91 <mdstrauss91@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,6 +68,7 @@ const config = {
                 "readingoutput.md",
                 "faq.md",
                 "overview.md" ,
+                "slsapolicies.md",
                 "glossary.md",
                 "README.md",
               ],
@@ -75,7 +76,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          //editUrl:
+          // editUrl:
           //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
