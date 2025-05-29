927465abf8d97a908a4060b714bcd2e9f23a47e8
houdini91 <mdstrauss91@gmail.com>
2025-05-29 09:14:03 +0300

    update files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,6 +98,7 @@ const config = {
                 "guides/secure-sfw-slsa/basic-examples.md",
                 // "guides/secure-sfw-slsa/cosign-keyverno.md",
                 "guides/secure-sfw-slsa/slsapolicies.md",
+                "guides/enforcing-sdlc-initiative.md",
                 "guides/ssdf-compliance/README.md",
                 "guides/ssdf-compliance/ssdfpolicies.md",
                 "guides/enforcing-sdlc-policy.md",
@@ -169,6 +170,9 @@ const config = {
                 "platforms/usage.md",
                 "platforms/hooks.md",
                 "release-notes/rns.md",
+                "configuration/**/*.md",
+                "configuration/*.md",
+
 
                 // "advanced-guide/generating-sboms.md",
                 // "advanced-guide/source-code-integrity.md",
